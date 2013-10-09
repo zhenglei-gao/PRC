7e602c67049c043a1325945a6c4bd72a94286b4b
Zhenglei Gao <zhenglei.gao@gmail.com>
2013-10-09 16:12:26 +0200

    first attempt

---
+++ simulatedEx/simple.sol
@@ -0,0 +1,272 @@
+ PRC small example: 6 species                                                    
+  RDA	 Canonical axes:	 1	 Covariables:	  3	 Scaling:	-1	          	          
+ Cent./stand.	by samples:	 0	 0	by species:	 1	 0
+ Log-transformation
+ Spec: Species scores                                                            
+
+    N	      NAME	    AX1    	  AX2    	  AX3    	  AX4   	   WEIGHT	     1
+
+    	          EIG 	 0.6198	   0.0000	   0.0000	   0.0000
+
+    1	 Sp1_ b=-1 	   0.7386	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+    2	 Sp2_ b=0  	   0.0000	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+    3	 Sp3_ b=1  	  -0.7385	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+    4	 Sp4_ b=1  	  -0.7385	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+    5	 Sp5_ b=2  	  -1.4771	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+    6	 Sp6_ b=2  	  -1.4771	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+  
+
+
+
+
+ PRC small example: 6 species                                                    
+  RDA	 Canonical axes:	 1	 Covariables:	  3	 Scaling:	-1	          	          
+ Cent./stand.	by samples:	 0	 0	by species:	 1	 0
+ Log-transformation
+ Samp: Sample scores                                                             
+
+    N	      NAME	    AX1    	  AX2    	  AX3    	  AX4   	   WEIGHT	     1
+
+    	          EIG 	 0.6198	   0.0000	   0.0000	   0.0000
+
+    1	  C1-W0    	   0.0000	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+    2	  C2-W0    	   0.0000	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+    3	  L1-W0    	   0.0000	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+    4	  H1-W0    	   0.0000	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+    5	  H2-W0    	   0.0000	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+    6	  C1-W1    	  -0.6950	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+    7	  C2-W1    	  -0.6950	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+    8	  L1-W1    	   0.1330	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+    9	  H1-W1    	   0.6285	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   10	  H2-W1    	   0.6285	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   11	  C1-W2    	  -1.1943	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   12	  C2-W2    	  -1.1943	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   13	  L1-W2    	  -0.3664	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   14	  H1-W2    	   1.3775	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   15	  H2-W2    	   1.3775	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   16	  C1-W3    	  -0.8363	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   17	  C2-W3    	  -0.8363	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   18	  L1-W3    	  -0.4454	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   19	  H1-W3    	   1.0591	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   20	  H2-W3    	   1.0591	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   21	  ORIGIN   	   7.5788	   0.0000	   0.0000	   0.0000	     0.00	     0.00
+  
+
+
+
+
+ PRC small example: 6 species                                                    
+  RDA	 Canonical axes:	 1	 Covariables:	  3	 Scaling:	-1	          	          
+ Cent./stand.	by samples:	 0	 0	by species:	 1	 0
+ Log-transformation
+ CFit: Cumulative fit per species as fraction of variance of species             
+
+    N	      NAME	    AX1    	  AX2    	  AX3    	  AX4   	   VAR(y)	% EXPL
+
+    	 FR FITTED    	 0.6198	   0.0000	   0.0000	   0.0000
+
+    1	 Sp1_ b=-1 	   0.5205	   0.0000	   0.0000	   0.0000	     0.65	    52.05
+    2	 Sp2_ b=0  	   0.0000	   0.0000	   0.0000	   0.0000	     0.06	     0.00
+    3	 Sp3_ b=1  	   0.8040	   0.0000	   0.0000	   0.0000	     0.42	    80.40
+    4	 Sp4_ b=1  	   0.4528	   0.0000	   0.0000	   0.0000	     0.75	    45.28
+    5	 Sp5_ b=2  	   0.8294	   0.0000	   0.0000	   0.0000	     1.63	    82.94
+    6	 Sp6_ b=2  	   0.5433	   0.0000	   0.0000	   0.0000	     2.49	    54.33
+  
+
+
+
+
+ PRC small example: 6 species                                                    
+  RDA	 Canonical axes:	 1	 Covariables:	  3	 Scaling:	-1	          	          
+ Cent./stand.	by samples:	 0	 0	by species:	 1	 0
+ Log-transformation
+ Regr: Regression/canonical coefficients for standardized variables              
+
+    N	      NAME	    AX1    	  AX2    	  AX3    	  AX4   	
+
+    	          EIG 	 0.6198	   0.0000	   0.0000	   0.0000
+
+    8	  Low*Wk0  	   0.0000	   0.0000	   0.0000	   0.0000
+    9	  Hig*Wk0  	   0.0000	   0.0000	   0.0000	   0.0000
+   10	  Low*Wk1  	   0.1805	   0.0000	   0.0000	   0.0000
+   11	  Hig*Wk1  	   0.3970	   0.0000	   0.0000	   0.0000
+   12	  Low*Wk2  	   0.1805	   0.0000	   0.0000	   0.0000
+   13	  Hig*Wk2  	   0.7716	   0.0000	   0.0000	   0.0000
+   14	  Low*Wk3  	   0.0852	   0.0000	   0.0000	   0.0000
+   15	  Hig*Wk3  	   0.5686	   0.0000	   0.0000	   0.0000
+  
+
+
+
+
+ PRC small example: 6 species                                                    
+  RDA	 Canonical axes:	 1	 Covariables:	  3	 Scaling:	-1	          	          
+ Cent./stand.	by samples:	 0	 0	by species:	 1	 0
+ Log-transformation
+ tVal: t-values of regression coefficients                                       
+
+    N	      NAME	    AX1    	  AX2    	  AX3    	  AX4   	
+
+    	 FR EXPLAINED 	 1.0000	   0.0000	   0.0000	   0.0000
+
+    8	  Low*Wk0  	   0.0000	   0.0000	   0.0000	   0.0000
+    9	  Hig*Wk0  	   0.0000	   0.0000	   0.0000	   0.0000
+   10	  Low*Wk1  	*********	   0.0000	   0.0000	   0.0000
+   11	  Hig*Wk1  	*********	   0.0000	   0.0000	   0.0000
+   12	  Low*Wk2  	*********	   0.0000	   0.0000	   0.0000
+   13	  Hig*Wk2  	*********	   0.0000	   0.0000	   0.0000
+   14	  Low*Wk3  	*********	   0.0000	   0.0000	   0.0000
+   15	  Hig*Wk3  	*********	   0.0000	   0.0000	   0.0000
+  
+
+
+
+
+ PRC small example: 6 species                                                    
+  RDA	 Canonical axes:	 1	 Covariables:	  3	 Scaling:	-1	          	          
+ Cent./stand.	by samples:	 0	 0	by species:	 1	 0
+ Log-transformation
+ StBi: Species coordinates for t-value biplot                                    
+
+    N	      NAME	    AX1    	  AX2    	  AX3    	  AX4   	   %(E+C)	  %(E)
+
+    	          EIG 	 0.6198	   0.0000	   0.0000	   0.0000
+
+    1	 Sp1_ b=-1 	   0.0000	   0.0000	   0.0000	   0.0000	   100.00	    52.05
+    2	 Sp2_ b=0  	   0.0000	   0.0000	   0.0000	   0.0000	   100.00	     0.00
+    3	 Sp3_ b=1  	   0.0000	   0.0000	   0.0000	   0.0000	   100.00	    80.40
+    4	 Sp4_ b=1  	   0.0000	   0.0000	   0.0000	   0.0000	   100.00	    45.28
+    5	 Sp5_ b=2  	   0.0000	   0.0000	   0.0000	   0.0000	   100.00	    82.94
+    6	 Sp6_ b=2  	   0.0000	   0.0000	   0.0000	   0.0000	   100.00	    54.33
+  
+
+
+
+
+ PRC small example: 6 species                                                    
+  RDA	 Canonical axes:	 1	 Covariables:	  3	 Scaling:	-1	          	          
+ Cent./stand.	by samples:	 0	 0	by species:	 1	 0
+ Log-transformation
+ EtBi: Environmental coordinates for t-value biplot                              
+
+    N	      NAME	    AX1    	  AX2    	  AX3    	  AX4   	
+
+    	          EIG 	 0.6198	   0.0000	   0.0000	   0.0000
+
+    8	  Low*Wk0  	   0.0000	   0.0000	   0.0000	   0.0000
+    9	  Hig*Wk0  	   0.0000	   0.0000	   0.0000	   0.0000
+   10	  Low*Wk1  	   0.1512	   0.0000	   0.0000	   0.0000
+   11	  Hig*Wk1  	   0.2959	   0.0000	   0.0000	   0.0000
+   12	  Low*Wk2  	   0.1512	   0.0000	   0.0000	   0.0000
+   13	  Hig*Wk2  	   0.5751	   0.0000	   0.0000	   0.0000
+   14	  Low*Wk3  	   0.0714	   0.0000	   0.0000	   0.0000
+   15	  Hig*Wk3  	   0.4238	   0.0000	   0.0000	   0.0000
+  
+
+
+
+
+ PRC small example: 6 species                                                    
+  RDA	 Canonical axes:	 1	 Covariables:	  3	 Scaling:	-1	          	          
+ Cent./stand.	by samples:	 0	 0	by species:	 1	 0
+ Log-transformation
+ CorE: Inter set correlations of environmental variables with axes               
+
+    N	      NAME	    AX1    	  AX2    	  AX3    	  AX4   	
+
+    	 FR EXTRACTED 	 0.1192	   0.0000	   0.0000	   0.0000
+
+    8	  Low*Wk0  	   0.0000	   0.0000	   0.0000	   0.0000
+    9	  Hig*Wk0  	   0.0000	   0.0000	   0.0000	   0.0000
+   10	  Low*Wk1  	   0.0422	   0.0000	   0.0000	   0.0000
+   11	  Hig*Wk1  	   0.3259	   0.0000	   0.0000	   0.0000
+   12	  Low*Wk2  	  -0.1163	   0.0000	   0.0000	   0.0000
+   13	  Hig*Wk2  	   0.7143	   0.0000	   0.0000	   0.0000
+   14	  Low*Wk3  	  -0.1414	   0.0000	   0.0000	   0.0000
+   15	  Hig*Wk3  	   0.5492	   0.0000	   0.0000	   0.0000
+  
+
+
+
+
+ PRC small example: 6 species                                                    
+  RDA	 Canonical axes:	 1	 Covariables:	  3	 Scaling:	-1	          	          
+ Cent./stand.	by samples:	 0	 0	by species:	 1	 0
+ Log-transformation
+ BipE: Biplot scores of environmental variables                                  
+
+    N	      NAME	    AX1    	  AX2    	  AX3    	  AX4   	
+
+    	 R(SPEC,ENV)  	 1.0000	   0.0000	   0.0000	   0.0000
+
+    8	  Low*Wk0  	   0.0000	   0.0000	   0.0000	   0.0000
+    9	  Hig*Wk0  	   0.0000	   0.0000	   0.0000	   0.0000
+   10	  Low*Wk1  	   0.0305	   0.0000	   0.0000	   0.0000
+   11	  Hig*Wk1  	   0.2095	   0.0000	   0.0000	   0.0000
+   12	  Low*Wk2  	  -0.0841	   0.0000	   0.0000	   0.0000
+   13	  Hig*Wk2  	   0.4592	   0.0000	   0.0000	   0.0000
+   14	  Low*Wk3  	  -0.1022	   0.0000	   0.0000	   0.0000
+   15	  Hig*Wk3  	   0.3530	   0.0000	   0.0000	   0.0000
+  
+
+
+
+
+ PRC small example: 6 species                                                    
+  RDA	 Canonical axes:	 1	 Covariables:	  3	 Scaling:	-1	          	          
+ Cent./stand.	by samples:	 0	 0	by species:	 1	 0
+ Log-transformation
+ CenE: Centroids of environmental variables (mean.gt.0) in ordination diagram    
+
+    N	      NAME	    AX1    	  AX2    	  AX3    	  AX4   	
+
+    	 R(SPEC,ENV)  	 1.0000	   0.0000	   0.0000	   0.0000
+
+    8	  Low*Wk0  	   0.0000	   0.0000	   0.0000	   0.0000
+    9	  Hig*Wk0  	   0.0000	   0.0000	   0.0000	   0.0000
+   10	  Low*Wk1  	   0.1330	   0.0000	   0.0000	   0.0000
+   11	  Hig*Wk1  	   0.6285	   0.0000	   0.0000	   0.0000
+   12	  Low*Wk2  	  -0.3664	   0.0000	   0.0000	   0.0000
+   13	  Hig*Wk2  	   1.3775	   0.0000	   0.0000	   0.0000
+   14	  Low*Wk3  	  -0.4454	   0.0000	   0.0000	   0.0000
+   15	  Hig*Wk3  	   1.0591	   0.0000	   0.0000	   0.0000
+  
+
+
+
+
+ PRC small example: 6 species                                                    
+  RDA	 Canonical axes:	 1	 Covariables:	  3	 Scaling:	-1	          	          
+ Cent./stand.	by samples:	 0	 0	by species:	 1	 0
+ Log-transformation
+ SamE: Sample scores which are linear combinations of environmental variables    
+
+    N	      NAME	    AX1    	  AX2    	  AX3    	  AX4   	   WEIGHT	     1
+
+    	          EIG 	 0.6198	   0.0000	   0.0000	   0.0000
+
+    1	  C1-W0    	   0.0000	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+    2	  C2-W0    	   0.0000	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+    3	  L1-W0    	   0.0000	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+    4	  H1-W0    	   0.0000	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+    5	  H2-W0    	   0.0000	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+    6	  C1-W1    	  -0.6950	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+    7	  C2-W1    	  -0.6950	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+    8	  L1-W1    	   0.1330	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+    9	  H1-W1    	   0.6285	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   10	  H2-W1    	   0.6285	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   11	  C1-W2    	  -1.1943	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   12	  C2-W2    	  -1.1943	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   13	  L1-W2    	  -0.3664	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   14	  H1-W2    	   1.3775	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   15	  H2-W2    	   1.3775	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   16	  C1-W3    	  -0.8363	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   17	  C2-W3    	  -0.8363	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   18	  L1-W3    	  -0.4454	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   19	  H1-W3    	   1.0591	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+   20	  H2-W3    	   1.0591	   0.0000	   0.0000	   0.0000	     1.00	     1.00
+  
+
+
+
+
